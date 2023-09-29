53d08672ea0759141c2357ec2e58a537db712718
Robert Cosimo Marra <108341388+ric3ing@users.noreply.github.com>
2023-09-29 11:22:01 +0200

    Add files via upload

---
+++ Contatto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marra.RobertCosimo._4i.rubrica
+{
+    internal class Contatto
+    {
+        private int numero;
+        private string nome;
+        private string cognome;
+
+        public int Numero { get => numero; set => numero = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Cognome { get => cognome; set => cognome = value; }
+    }
+}
